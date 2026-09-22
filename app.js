eb2d78386591c1f7b2e1fd711270380282020cc3
Anushree Kekapure <annushreek08@gmail.com>
2026-09-22 16:29:15 +0530

    Add custom middleware and welcome route

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const app = express();
 const PORT = 3000;
 
 
+// Custom Middleware
+const userMiddleware = (req, res, next) => {
+    req.user = "Guest";
+    next();
+};
+
+
+// Welcome Route
+app.get("/welcome", userMiddleware, (req, res) => {
+    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
+
+// Start Server
 app.listen(PORT, () => {
     console.log(
         "Server is up and running on port 3000! Ready to handle requests."
