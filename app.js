0a8753e94f35590f9722fb172acfa3ff5d93c44c
Anushree Kekapure <annushreek08@gmail.com>
2026-09-24 15:17:45 +0530

    Implement Express Router

--- app.js
+++ app.js
@@ -2,40 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 4000;
+const PORT = 3000;
 
+const routes = require("./routes/routes");
 
-// GET /products
-app.get("/products", (req, res) => {
-    res.send("Here is the list of all products.");
-});
+app.use("/", routes);
 
-
-// POST /products
-app.post("/products", (req, res) => {
-    res.send("A new product has been added.");
-});
-
-
-// GET /categories
-app.get("/categories", (req, res) => {
-    res.send("Here is the list of all categories.");
-});
-
-
-// POST /categories
-app.post("/categories", (req, res) => {
-    res.send("A new category has been created.");
-});
-
-
-// Wildcard / 404 route
-app.use((req, res) => {
-    res.status(404).send("<h1>404 - Page Not Found</h1>");
-});
-
-
-// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ routes/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/users", (req, res) => {
+    res.send("Here is the list of all users.");
+});
+
+router.post("/users", (req, res) => {
+    res.send("A new user has been added.");
+});
+
+router.get("/orders", (req, res) => {
+    res.send("Here is the list of all orders.");
+});
+
+router.post("/orders", (req, res) => {
+    res.send("A new order has been created.");
+});
+
+module.exports = router;
